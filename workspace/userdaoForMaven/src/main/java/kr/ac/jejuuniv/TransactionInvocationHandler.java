1ac69a1109e2e22d3bbc938de78f56dcce9a5774
허윤호 <hyh0408@gmail.com>
2014-05-11 18:46:12 +0900

    aop1

---
+++ workspace/userdaoForMaven/src/main/java/kr/ac/jejuuniv/TransactionInvocationHandler.java
@@ -0,0 +1,14 @@
+package kr.ac.jejuuniv;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class TransactionInvocationHandler implements InvocationHandler{
+
+	@Override
+	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
